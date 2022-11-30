3c7b7424a77b64615069a82b045e6867c6c1934c
vasanth064 <vasanthvdev@gmail.com>
2022-11-30 16:55:15 +0530

    added cahing support

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 const container = document.querySelector('.container');
 const button = document.querySelector('#button');
-const searchProviderImage = document.querySelector('#searchProviderImage'); 
+const searchProviderImage = document.querySelector('#searchProviderImage');
 const searchProviderLink = document.querySelector('#searchProviderForm');
+const searchField = document.querySelector('#searchField');
 
 navigator.serviceWorker.register('./sw.js');
 const select = document.querySelector('select');
-searchProvider.forEach((provider)=>{
-    const newOption = document.createElement('option');
-    const optionText = document.createTextNode(provider.title);
-    newOption.appendChild(optionText);
-    newOption.setAttribute("value",provider.title);
-    select.appendChild(newOption);
+
+searchProviderLink.addEventListener('submit', function (event) {
+  if (searchField.value.includes('.')) {
+    event.preventDefault();
+    window.location.href = `http://${searchField.value}`;
+  }
 });
 
+searchProvider.forEach((provider) => {
+  const newOption = document.createElement('option');
+  const optionText = document.createTextNode(provider.title);
+  newOption.appendChild(optionText);
+  newOption.setAttribute('value', provider.title);
+  select.appendChild(newOption);
+});
 
-function changeSearchProvider(){
-    const searchProviderName = select.value;
-    const provider = searchProvider.filter((provider)=> provider.title == searchProviderName);
-    console.log(provider);
-    localStorage.setItem('index',provider[0].index);
-    localStorage.setItem('title',provider[0].title);
-    localStorage.setItem('link',provider[0].link);
-    localStorage.setItem('image',provider[0].image);
-    searchProviderImage.setAttribute('src',localStorage.getItem('image'))
-    searchProviderLink.setAttribute('action',localStorage.getItem('link'))
+function changeSearchProvider() {
+  const searchProviderName = select.value;
+  const provider = searchProvider.filter(
+    (provider) => provider.title == searchProviderName
+  );
+  console.log(provider);
+  localStorage.setItem('index', provider[0].index);
+  localStorage.setItem('title', provider[0].title);
+  localStorage.setItem('link', provider[0].link);
+  localStorage.setItem('image', provider[0].image);
+  searchProviderImage.setAttribute('src', localStorage.getItem('image'));
+  searchProviderLink.setAttribute('action', localStorage.getItem('link'));
 }
-
 
 data.forEach((row) => {
   const containerRow = document.createElement('div');
@@ -58,6 +67,14 @@ data.forEach((row) => {
     }
   });
 });
-searchProviderImage.setAttribute('src',localStorage.getItem('image'))
-searchProviderLink.setAttribute('action',localStorage.getItem('link'))
-document.querySelector("select").selectedIndex = parseInt(localStorage.getItem('index'))
+searchProviderImage.setAttribute(
+  'src',
+  localStorage.getItem('image') || './images/google.webp'
+);
+searchProviderLink.setAttribute(
+  'action',
+  localStorage.getItem('link') || 'http://www.google.com/search'
+);
+document.querySelector('select').selectedIndex = parseInt(
+  localStorage.getItem('index')
+);
